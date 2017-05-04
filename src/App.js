69a58c424447c69d064802369c9595391801dcd9
Mike Ziccardi <mikeziccardi@gmail.com>
2017-05-04 09:33:50 -0600

    adds axios and debugger button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  debugg(){
+    debugger
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={(e)=>this.debugg(e)}>debugger</button>
       </div>
     );
   }
